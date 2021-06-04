2895c7657645fe7a4a30411aa01b07cbd12f965d
Richard Post <richard.hn.post@gmail.com>
2021-06-04 10:01:02 +0200

    Hide slideshow when there are no pictures

--- Views/Accommodation.php
+++ Views/Accommodation.php
@@ -3,35 +3,38 @@
 <div class="main">
     <div class="accommodation-title"><?= $house["title"]; ?></div>
 
-    <div class="slideshow-container">
-        <?php
-        if (count($files) > 0) {
+    <?php
+    if (count($files) > 0) :
+    ?>
+        <div class="slideshow-container">
+            <?php
             foreach ($files as $file) :
-        ?>
+            ?>
 
                 <div class="mySlides fade">
                     <div class="img" style="background-image: url(<?= $file['path'] ?>);"></div>
                 </div>
 
-        <?php
+            <?php
             endforeach;
-        }
-        ?>
+            ?>
 
-        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-        <a class="next" onclick="plusSlides(1)">&#10095;</a>
-    </div>
-    <br>
+            <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+            <a class="next" onclick="plusSlides(1)">&#10095;</a>
+        </div>
+        <br>
 
-    <div style="text-align:center">
-        <?php
-        for ($i = 0; $i < count($files); $i++) :
-        ?>
+        <div style="text-align:center">
+            <?php
+            for ($i = 0; $i < count($files); $i++) :
+            ?>
 
-            <span class="dot" onclick="currentSlide(<?= $i + 1 ?>)"></span>
+                <span class="dot" onclick="currentSlide(<?= $i + 1 ?>)"></span>
 
-        <?php endfor; ?>
-    </div>
+            <?php endfor; ?>
+        </div>
+
+    <?php endif; ?>
 </div>
 
 <?php include "Resources/Includes/footer.inc.php"; ?>
